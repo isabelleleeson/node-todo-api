05f4126a82dd0673fcf1aa054b6b890004d4724c
Isabelle Leeson <isabelleleeson@Isabelles-MacBook-Pro.local>
2017-02-04 13:43:08 -0500

    First commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,40 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb') // same as above
+
+var obj = new ObjectID();
+console.log(obj);
+
+// var user = {name: 'andrew', age: 25};
+// var {name} = user; //ES6
+// console.log(name); object destructering
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Toni',
+    age: 23,
+    location: 'ICON'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+    console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  });
+
+  db.close();
+}); // can be heroku, amazon web services
